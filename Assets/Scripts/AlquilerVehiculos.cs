c638161265a278c64a0d2a876a08225dcd84c67b
Tomas Hauser <142511066+Hauserman@users.noreply.github.com>
2024-06-03 09:30:49 -0300

    terminado

--- Assets/Scripts/AlquilerVehiculos.cs
+++ Assets/Scripts/AlquilerVehiculos.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class AlquilerVehiculos : MonoBehaviour
 {
     public Vehiculo[] vehiculos;
+    float precioactual;
+    float total;
 
     // Start is called before the first frame update
     void Start()
     {
-        ResetearVehiculos();
+        for (int i = 0; i < vehiculos.Length; i++)
+        {
+            if (vehiculos[i].cantRuedas > 3)
+            {
+                vehiculos[i].CartelPromo.gameObject.SetActive(false);
+            }
+            
+        }
+
+        for (int i = 0; i < vehiculos.Length; i++)
+        {
+            precioactual = vehiculos[i].precioAlquiler;
+            total += precioactual;
+        }
+        total = total / vehiculos.Length;
+        Debug.Log("Promedio de precio de alquiler es: " + total);
+
+
     }
 
     // Update is called once per frame
@@ -17,12 +36,20 @@ public class AlquilerVehiculos : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-
+           ResetearVehiculos();
         }   
     }
 
     void ResetearVehiculos()
     {
+        int randomnum = Random.Range(0, vehiculos.Length);
+        
+        for (int i = 0; i < vehiculos.Length; i++)
+        {
+            vehiculos[i].CartelPromo.gameObject.SetActive(false);
+        }
+
+        vehiculos[randomnum].CartelPromo.gameObject.SetActive(true);
 
     }
 }
