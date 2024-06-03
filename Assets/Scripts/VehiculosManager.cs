e9120aff29ea457f27f878d4d92409a43f631079
Tomas Hauser <142511066+Hauserman@users.noreply.github.com>
2024-06-03 09:05:20 -0300

    aaa

---
+++ Assets/Scripts/VehiculosManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehiculosManager : MonoBehaviour
+{
+
+    public Vehiculo vehiculoscript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int ruedas = vehiculoscript.cantRuedas;
+        Debug.Log("Cant ruedas: " + ruedas);
+        if (ruedas < 6)
+        {
+            vehiculoscript.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
